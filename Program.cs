bd516b5a3cd3c7a33bc870db34b2632681945d6c
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-04-10 01:53:57 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número con decimales");
+            decimal numero = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"El número ingresado es {numero} \r\n" +
+                "y el número redondeado en dos decimales es " + Math.Round(numero, 2));
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
