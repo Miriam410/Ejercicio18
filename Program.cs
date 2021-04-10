4e6adbabcfd970044423b9940bf8c60eb8d89ba7
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-04-10 02:16:24 -0300

    Faltó la validación.

--- Program.cs
+++ Program.cs
@@ -10,11 +10,26 @@ namespace Ejercicio18
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ingrese un número con decimales");
-            decimal numero = Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine($"El número ingresado es {numero} \r\n" +
-                "y el número redondeado en dos decimales es " + Math.Round(numero, 2));
-            Console.WriteLine("Presione una tecla para salir");
+            string numero;
+            bool Flag = false;
+            decimal Salida=0;
+            do
+            {
+                Console.WriteLine("Ingrese un número con decimales");
+                numero = Console.ReadLine();
+                if (!decimal.TryParse(numero, out Salida))
+                {
+                    Console.WriteLine("Usted debe ingresar un número");
+                }
+                else
+                {
+                    Flag = true;
+                    Console.WriteLine($"El número ingresado es {numero} \r\n" +
+                    "y el número redondeado en dos decimales es " + Math.Round(Salida, 2));
+                    Console.WriteLine("Presione una tecla para salir");
+                }
+            } while (Flag==false);
+            
             Console.ReadKey();
         }
     }
